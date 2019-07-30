ef6ab860846e3d8e7d67d78d729e5f4b8b26f82f
Vasu Kulkarni <vasu@redhat.com>
2019-07-30 15:35:13 -0700

    Ripsaw class to run various workloads and scale tests
    Signed-off-by: Vasu Kulkarni <vasu@redhat.com>

---
+++ ocs_ci/ocs/ripsaw.py
@@ -0,0 +1,92 @@
+
+"""
+RipSaw Class to run various workloads and scale tests
+"""
+import logging
+import tempfile
+
+from ocs_ci.ocs.exceptions import CommandFailed
+from ocs_ci.ocs.ocp import OCP
+from subprocess import run, CalledProcessError
+from ocs_ci.utility.utils import run_cmd
+
+log = logging.getLogger(__name__)
+
+
+class RipSaw(object):
+    """
+      Workload operation using RipSaw
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Initializer function
+
+        Args:
+            kwargs (dict):
+                Following kwargs are valid
+                repo: Ripsaw repo to used - a github link
+                branch: branch to use from the repo
+                namespace: namespace for the operator
+
+        Example Usage:
+            r1 = RipSaw()
+            r1.apply_crd(crd='ripsaw_v1alpha1_ripsaw_crd.yaml')
+            # use oc apply to apply custom modified bench
+            my_custom_bench = my_custom_bench.yaml
+            run_cmd('oc apply -f my_custom_bench')
+        """
+        self.args = kwargs
+        self.repo = self.args.get('repo', 'https://github.com/cloud-bulldozer/ripsaw')
+        self.branch = self.args.get('branch', 'master')
+        self.namespace = self.args.get('namespace', 'ripsaw')
+        self.ocp = OCP()
+        self.ns_obj = OCP(kind='namespace')
+        self._create_namespace()
+        self._clone_ripsaw()
+
+    def _create_namespace(self):
+        """
+        create namespace for RipSaw
+        """
+        self.ocp.new_project(self.namespace)
+
+    def _clone_ripsaw(self):
+        """
+        clone the ripaw repo
+        """
+        self.dir = tempfile.mkdtemp(prefix='ripsaw_')
+        try:
+            log.info(f'cloning ripsaw in {self.dir}')
+            git_clone_cmd = f'git clone -b {self.branch} {self.repo} '
+            run(
+                git_clone_cmd,
+                shell=True,
+                cwd=self.dir,
+                check=True
+            )
+            self.crd = 'resources/crd/'
+            self.operator = 'resources/operator.yaml'
+        except (CommandFailed, CalledProcessError)as cf:
+            log.error('Error during cloning of ripsaw repository')
+            raise cf
+
+    def apply_crd(self, crd):
+        """
+        Apply the CRD
+
+        Args:
+            crd (str): Name of file to apply
+        """
+        self.crd = crd
+        self.dir += '/ripsaw'
+        run(f'oc apply -f deploy', shell=True, check=True, cwd=self.dir)
+        run(f'oc apply -f {crd}', shell=True, check=True, cwd=self.dir)
+        run(f'oc apply -f {self.operator}', shell=True, check=True, cwd=self.dir)
+
+    def cleanup(self):
+        run_cmd(f'cd {self.dir.name} ; oc delete -f {self.crd}')
+        run_cmd(f'cd {self.dir.name} ; oc delete -f {self.operator}')
+        run_cmd(f'cd {self.dir.name} ; oc delete -f deploy')
+        run_cmd(f'oc delete project {self.namespace}')
+        self.ns_obj.wait_for_delete(resource_name=self.namespace)
